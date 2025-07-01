c282de5563d5366b10ffc35fd8b2f0fc943021aa
H3llFireX <SpicyMexicanIV@gmail.com>
2025-07-01 14:03:41 -0400

    Update UI.lua

--- GUI/UI.lua
+++ GUI/UI.lua
@@ -1,1 +1,22 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/<your-username>/HellfireX-Aimbot/main/Libs/PepsiUILibrary.lua"))()
+local Window = Library:CreateWindow({ Name = "HellfireX", Themeable = { Image = "", Info = "Private Dev Build" }})
 
+local SettingsTab = Window:CreateTab({ Name = "Aimbot Settings" })
+local Section = SettingsTab:CreateSection({ Name = "Main" })
+
+Section:AddToggle({
+    Name = "Aimbot Enabled",
+    Value = true,
+    Callback = function(v)
+        getgenv().Aimbot.Settings.Enabled = v
+    end
+})
+
+Section:AddDropdown({
+    Name = "Lock Part",
+    Value = "Head",
+    List = { "Head", "HumanoidRootPart" },
+    Callback = function(v)
+        getgenv().Aimbot.Settings.LockPart = v
+    end
+})
